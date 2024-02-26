175b1c9b39eded23e9059b51ac1648ecf5209bf0
Le1NZ <pokrovskiypasha@yandex.ru>
2024-02-26 03:13:40 +0300

    Added role of admins. They can delete user from queues, make others admins and switch the queue

--- app/src/main/java/ru/pokrovskii/queue/domain/DTO/QueueDTO.kt
+++ app/src/main/java/ru/pokrovskii/queue/domain/DTO/QueueDTO.kt
@@ -2,5 +2,6 @@ package ru.pokrovskii.queue.domain.DTO
 
 data class QueueDTO(
     val id: String = "",
-    val name: String = ""
+    val name: String = "",
+    var admin: Boolean = false,
 )
--- app/src/main/java/ru/pokrovskii/queue/domain/DTO/UserDTO.kt
+++ app/src/main/java/ru/pokrovskii/queue/domain/DTO/UserDTO.kt
@@ -3,4 +3,5 @@ package ru.pokrovskii.queue.domain.DTO
 data class UserDTO(
     val id: String = "",
     var name: String = "",
+    var admin: Boolean = false
 )
